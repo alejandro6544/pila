21ce1aeb25124b850b9608f98cff9399d67a1f53
dacastro42 <108814849+dacastro42@users.noreply.github.com>
2022-08-09 10:07:38 -0500

    Initial commit

---
+++ src/vista/MainPila.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import datos.Productos;
+import modelo.Pila;
+
+/**
+ *
+ * @author pc
+ */
+public class MainPila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Pila<Productos> pilaP=new Pila<>();
+        pilaP.apilar(new Productos("arroz", 2, 5000));
+        pilaP.apilar(new Productos("Panalhuevos", 1, 14000));
+        pilaP.apilar(new Productos("carne", 2, 15000));
+        pilaP.apilar(new Productos("panela", 2, 8000));
+        
+        
+        System.out.println("Pila \n"+pilaP.toString());
+        
+        pilaP.desapilar();
+        System.out.println("Pila \n"+pilaP.toString());
+        
+    }
+    
+}
